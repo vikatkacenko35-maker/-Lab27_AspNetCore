88e8f0f89b3ac034f632b95eaa61915870e0353b
karasidnez <karasdnez@gmail.com>
2026-04-10 11:36:24 +0300

    Step 8: middleware logging and custom headers

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -3,6 +3,31 @@ using System.Data;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+
+app.Use(async (context, next) =>
+{
+    System.Console.WriteLine($"[LOG] {context.Request.Method} {context.Request.Path}");
+    await next(context);
+    var key = context.Request.Query["key"];
+    if (key != "secret" || key == "")
+    {
+        context.Response.StatusCode = 401;
+        await context.Response.WriteAsync("Unauthorized: Invalid credentials.401");
+        System.Console.WriteLine($"[LOG] Ответ отправлен: {context.Response.StatusCode}");
+        return;
+    }else{    
+        System.Console.WriteLine($"[LOG] Ответ отправлен: {context.Response.StatusCode}");
+}
+    await next(context);
+});
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("X-Powered-By", "ASP.NET Core Lab27");
+    await next(context);
+});
+
+
 app.MapGet("/", () => "Привет от ИСП-231! Автор: Виктория Ткаченко");
 app.MapGet("/about", () => "Это мой первый такой сервер");
 
@@ -31,6 +56,7 @@ app.MapGet("/product/{id}", (int id) => new Product(
     Price: id * 99.99m,
     InStock: id % 2 == 0
 ));
+
 
 app.Run();
 record Product(int id, string Name, decimal Price, bool InStock);
